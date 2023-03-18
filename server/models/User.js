6abf59746f8baa5d5a103a13f2b74fed522c9207
adityachandra1 <adityachandra9898@gmail.com>
2023-03-18 10:42:46 +0530

    user-mock

--- server/models/User.js
+++ server/models/User.js
@@ -32,6 +32,9 @@ const userSchema = new mongoose.Schema(
         college: {
             type: String,
         },
+        role: {
+            type: String,
+        },
         password: {
             type: String,
             required: true,
